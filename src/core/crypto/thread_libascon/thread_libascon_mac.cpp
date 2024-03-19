3a4c08030925bada28279c91a24cd1112864874b
Simeon Tran <siatran@ucsc.edu>
2024-03-18 21:42:53 -0700

    Added code that uses libascon for thread

---
+++ src/core/crypto/thread_libascon/thread_libascon_mac.cpp
@@ -0,0 +1,190 @@
+#include "crypto/thread_ascon.hpp"
+#include "thread_ascon_encryption_flags.h"
+
+#include "openthread/platform/radio.h"
+#include "mac/mac_frame.hpp"
+#include "mac/mac_types.hpp"
+
+#include <inttypes.h>
+
+void ConvertToAsconKey(const ot::Mac::KeyMaterial &aMacKey, void *asconKey) {
+  ot::Crypto::Key cryptoKey;
+  aMacKey.ConvertToCryptoKey(cryptoKey);
+  if (cryptoKey.GetBytes() == NULL) {
+    otLogCritPlat("Failed to get Thread network key.");
+  }
+
+  EmptyMemory(asconKey, OT_NETWORK_KEY_SIZE);
+  memcpy(asconKey, cryptoKey.GetBytes(), OT_NETWORK_KEY_SIZE);
+
+  return;
+}
+
+namespace ot
+{
+namespace Mac
+{
+
+uint8_t* Frame::AddAddrToAd(Address addr, uint8_t* offset) {
+  if (addr.GetType() == Mac::Address::kTypeExtended) // 8 bytes
+  {
+    Mac::ExtAddress ext = addr.GetExtended();
+    memcpy(offset, ext.m8, sizeof(Mac::ExtAddress));
+    offset += sizeof(Mac::ExtAddress);
+  }
+  else if (addr.GetType() == Mac::Address::kTypeShort) // 2 bytes
+  {
+    Mac::ShortAddress shortAddr = addr.GetShort();
+    memcpy(offset, &shortAddr, sizeof(Mac::ShortAddress));
+    offset += sizeof(Mac::ShortAddress);
+  }
+  else
+  {
+    otLogCritPlat("Failed to get address to be used in the nonce.");
+  }
+  return offset;
+}
+
+void Frame::CreateAssocData(void *aAssocData) {
+  EmptyMemory(aAssocData, CRYPTO_ABYTES);
+
+  Address dst; EmptyMemory(&dst, sizeof(Address));
+  if (GetDstAddr(dst) != OT_ERROR_NONE) {
+    otLogCritPlat("Failed to get destination address.");
+  }
+
+  Address src; EmptyMemory(&src, sizeof(Address));
+  if (GetSrcAddr(src) != OT_ERROR_NONE) {
+    otLogCritPlat("Failed to get source address.");
+  }
+
+  uint8_t* offset = (uint8_t *) aAssocData;
+  offset = AddAddrToAd(dst, offset);
+  offset = AddAddrToAd(src, offset);
+
+  return;
+}
+
+void Frame::CreateAsconNonce(void* aNonce) {
+  EmptyMemory(aNonce, CRYPTO_NPUBBYTES);
+
+  uint8_t sequenceNumber = GetSequence();
+
+  uint8_t keyId = 0;
+  if (GetKeyId(keyId) != OT_ERROR_NONE) {
+    otLogCritPlat("Failed to get Key ID.");
+  };
+
+#if THREAD_ASCON_DEBUG
+  otLogNotePlat("Sequence Number: %" PRIu8 "", sequenceNumber);
+  otLogNotePlat("Key ID: %" PRIu8 "", keyId);
+  otLogNotePlat("Footer Bits: %" PRIu32 "", ((uint32_t *) GetFooter())[0]);
+#endif // THREAD_ASCON_DEBUG
+
+  uint8_t *offset = (uint8_t *) aNonce;
+
+  memcpy(offset, &sequenceNumber, sizeof(uint8_t));
+  offset += sizeof(uint8_t);
+
+  memcpy(offset, &keyId, sizeof(uint8_t));
+  offset += sizeof(uint8_t);
+
+  /**
+   * AesCcm::Finalize() generates the nonces by grabbing THE FIRST 4 BYTES OF
+   * THE FOOTER (i.e. GetFooterLength() - GetFcsSize() ). We do the same
+   * when generating the Associated Data.
+   *
+   * Looking at Wireshark, I believe the first 4 bytes of the footer
+   * is the MIC.
+  */
+  memcpy(offset, GetFooter(), GetFooterLength() - GetFcsSize());
+
+  return;
+}
+
+Error TxFrame::AsconDataEncrypt() {
+  unsigned char key[OT_NETWORK_KEY_SIZE];
+  ConvertToAsconKey(GetAesKey(), key);
+
+  unsigned char assocData[CRYPTO_ABYTES];
+  CreateAssocData(assocData);
+
+  unsigned char nonce[CRYPTO_NPUBBYTES];
+  CreateAsconNonce(nonce);
+
+#if THREAD_ASCON_DEBUG
+  AsconDebugPrint(key, nonce, assocData);
+#endif // THREAD_ASCON_DEBUG
+
+  uint8_t footerLength = GetFooterLength();
+  uint8_t footerCopy[footerLength];
+  memcpy(footerCopy, GetFooter(), footerLength);
+
+  uint16_t plaintextLength = GetPayloadLength();
+  unsigned long long ciphertextLength;
+
+  unsigned long long expectedCipherLen = plaintextLength + CRYPTO_ABYTES;
+
+  if (expectedCipherLen >= GetMaxPayloadLength()) {
+    otLogWarnPlat("Ciphertext is too big - not going to use ASCON encryption.");
+    return kErrorNoBufs;
+  }
+
+  SetPayloadLength(expectedCipherLen);
+
+  void* end = GetPayload() + GetPayloadLength();
+  memcpy(end, footerCopy, footerLength);
+
+  crypto_aead_encrypt(GetPayload(), &ciphertextLength,
+                      GetPayload(), plaintextLength,
+                      assocData, CRYPTO_ABYTES,
+                      NULL, nonce, key);
+
+  OT_ASSERT(expectedCipherLen == ciphertextLength);
+
+  SetIsSecurityProcessed(true);
+  return OT_ERROR_NONE;
+}
+
+Error RxFrame::AsconDataDecrypt(const KeyMaterial &aMacKey) {
+  unsigned char key[OT_NETWORK_KEY_SIZE];
+  ConvertToAsconKey(aMacKey, key);
+
+  unsigned char assocData[CRYPTO_ABYTES];
+  CreateAssocData(assocData);
+
+  unsigned char nonce[CRYPTO_NPUBBYTES];
+  CreateAsconNonce(nonce);
+
+#if THREAD_ASCON_DEBUG
+  AsconDebugPrint(key, nonce, assocData);
+#endif // THREAD_ASCON_DEBUG
+
+  uint8_t footerLength = GetFooterLength();
+  uint8_t footerCopy[footerLength];
+  memcpy(footerCopy, GetFooter(), footerLength);
+
+  uint16_t ciphertextLength = GetPayloadLength();
+  unsigned long long plaintextLength;
+  uint8_t plaintextBuffer[ciphertextLength - CRYPTO_ABYTES];
+
+  int status = crypto_aead_decrypt(plaintextBuffer, &plaintextLength, NULL,
+                                   GetPayload(), ciphertextLength,
+                                   assocData, CRYPTO_ABYTES,
+                                   nonce, key);
+  if (status != 0) {
+    otLogWarnPlat("Invalid ASCON ciphertext (MAC).");
+    return OT_ERROR_SECURITY;
+  }
+
+  memcpy(GetPayload(), plaintextBuffer, plaintextLength);
+  SetPayloadLength(plaintextLength);
+
+  void* end = GetPayload() + GetPayloadLength();
+  memcpy(end, footerCopy, footerLength);
+
+  return OT_ERROR_NONE;
+}
+
+} // namespace Mac
+} // namespace ot
---
+++ src/core/crypto/thread_libascon/thread_libascon_mle.cpp
@@ -0,0 +1,186 @@
+#include "crypto/thread_ascon.hpp"
+#include "thread/mle.hpp"
+#include "mac/mac_types.hpp"
+
+#include <inttypes.h>
+
+/**
+ * The MLE nonce will be composed of the IPv6 address of the sender,
+ * the MLE frame counter, and the Key ID mode of the packet.
+ *
+ * @param[in] sender: the IPv6 address of sender of the MLE packet
+ * @param[in] frameCounter: the frame counter of the MLE packet
+ * @param[in] keyId: the Key ID mode of the MLE packet
+ *
+ * @param[out] aNonce: the pointer to the nonce bytes
+*/
+void createNonce(ot::Ip6::Address sender,
+                 uint32_t frameCounter,
+                 uint32_t keyId,
+                 void* aNonce)
+{
+  EmptyMemory(aNonce, CRYPTO_NPUBBYTES);
+
+  ot::Mac::ExtAddress senderExt;
+  sender.GetIid().ConvertToExtAddress(senderExt);
+
+  uint8_t *offset = (uint8_t *) aNonce;
+
+  memcpy(offset, senderExt.m8, sizeof(ot::Mac::ExtAddress));
+  offset += sizeof(ot::Mac::ExtAddress);
+
+  memcpy(offset, &frameCounter, sizeof(uint32_t));
+  offset += sizeof(uint32_t);
+
+  memcpy(offset, &keyId, sizeof(uint32_t));
+  return;
+}
+
+/**
+ * The Associated Data consists of the sender and receiver 802.15.4
+ * extended addresses.
+ *
+ * @param[in] sender: the IPv6 address of the sender
+ * @param[in] receiver: the IPv6 address of the receiver
+ *
+ * @param[out] aAssocData: a pointer to the Associated Data bytes in memory
+*/
+void createAssocData(ot::Ip6::Address sender,
+                     ot::Ip6::Address receiver,
+                     void* aAssocData)
+{
+  EmptyMemory(aAssocData, CRYPTO_ABYTES);
+
+  ot::Mac::ExtAddress senderExt, receiverExt;
+  sender.GetIid().ConvertToExtAddress(senderExt); // 8 bytes
+  receiver.GetIid().ConvertToExtAddress(receiverExt); // 8 bytes
+
+  uint8_t *offset = reinterpret_cast<uint8_t *>(aAssocData);
+  memcpy(offset, senderExt.m8, OT_EXT_ADDRESS_SIZE);
+
+  offset += OT_EXT_ADDRESS_SIZE;
+  memcpy(offset, receiverExt.m8, OT_EXT_ADDRESS_SIZE);
+
+  return;
+}
+
+namespace ot
+{
+namespace Mle
+{
+
+#define ASCON_TAG_LENGTH CRYPTO_ABYTES
+
+Error Mle::AsconMleEncrypt(Message                &aMessage,
+                           const Ip6::MessageInfo &aMessageInfo,
+                           const SecurityHeader   &aHeader,
+                           uint16_t               aCmdOffset)
+{
+  otError error = OT_ERROR_NONE;
+
+  unsigned char key[OT_NETWORK_KEY_SIZE];
+  GetAsconKey(aHeader.GetKeyId(), key);
+
+  unsigned char assocData[CRYPTO_ABYTES];
+  createAssocData(aMessageInfo.GetSockAddr(), aMessageInfo.GetPeerAddr(),
+                  assocData);
+
+  unsigned char nonce[CRYPTO_NPUBBYTES];
+  createNonce(aMessageInfo.GetSockAddr(), aHeader.GetFrameCounter(),
+              aHeader.GetKeyId(), nonce);
+
+#if THREAD_ASCON_DEBUG
+  AsconDebugPrint(key, nonce, assocData);
+#endif // THREAD_ASCON_DEBUG
+
+  uint16_t payloadLen = aMessage.GetLength() - aCmdOffset;
+
+  // Read payload data from the Message.
+  uint8_t payload[payloadLen];
+  EmptyMemory(payload, payloadLen);
+  aMessage.ReadBytes(aCmdOffset, payload, payloadLen);
+
+  unsigned long long expectedCipherLen = payloadLen + ASCON_TAG_LENGTH;
+  uint8_t ciphertext[expectedCipherLen];
+  EmptyMemory(ciphertext, expectedCipherLen);
+
+  unsigned long long actualCipherLen;
+
+  crypto_aead_encrypt(ciphertext, &actualCipherLen,
+                      payload, payloadLen,
+                      assocData, CRYPTO_ABYTES,
+                      NULL, nonce, key);
+
+  OT_ASSERT(expectedCipherLen == actualCipherLen);
+
+  // Replace plaintext with ciphertext.
+  aMessage.WriteBytes(aCmdOffset, ciphertext, payloadLen);
+
+  uint8_t tag[ASCON_TAG_LENGTH];
+  uint8_t *tagOffset = ciphertext + payloadLen;
+  memcpy(&tag, tagOffset, ASCON_TAG_LENGTH);
+
+  // Add the ASCON tag at the end of the ciphertext.
+  error = aMessage.Append(tag);
+  if (error == kErrorNoBufs) {
+    otLogCritPlat("Cannot grow message to add tag in MLE packet.");
+  }
+
+  return error;
+}
+
+Error Mle::AsconMleDecrypt(Message                &aMessage,
+                           const Ip6::MessageInfo &aMessageInfo,
+                           const SecurityHeader   &aHeader,
+                           uint16_t                aCmdOffset)
+{
+  unsigned char key[OT_NETWORK_KEY_SIZE];
+  GetAsconKey(aHeader.GetKeyId(), key);
+
+  unsigned char assocData[CRYPTO_ABYTES];
+  createAssocData(aMessageInfo.GetPeerAddr(), aMessageInfo.GetSockAddr(),
+                  assocData);
+
+  unsigned char nonce[CRYPTO_NPUBBYTES];
+  createNonce(aMessageInfo.GetPeerAddr(), aHeader.GetFrameCounter(),
+              aHeader.GetKeyId(), nonce);
+
+#if THREAD_ASCON_DEBUG
+  AsconDebugPrint(key, nonce, assocData);
+#endif // THREAD_ASCON_DEBUG
+
+  uint16_t cipherLen = aMessage.GetLength() - aCmdOffset;
+
+  // Read the ciphertext payload.
+  uint8_t ciphertext[cipherLen];
+  EmptyMemory(ciphertext, cipherLen);
+  aMessage.ReadBytes(aCmdOffset, ciphertext, cipherLen);
+
+  unsigned long long expectedPayloadLen = cipherLen - ASCON_TAG_LENGTH;
+  uint8_t payload[expectedPayloadLen];
+  EmptyMemory(payload, expectedPayloadLen);
+
+  unsigned long long actualPayloadLen;
+  int status;
+
+  status = crypto_aead_decrypt(payload, &actualPayloadLen, NULL,
+                               ciphertext, cipherLen,
+                               assocData, CRYPTO_ABYTES, nonce, key);
+  if (status != 0) {
+    otLogWarnPlat("Invalid ASCON ciphertext (MLE).");
+    return OT_ERROR_SECURITY;
+  }
+
+  OT_ASSERT(actualPayloadLen == expectedPayloadLen);
+
+  // Replace ciphertext with plaintext.
+  aMessage.WriteBytes(aCmdOffset, payload, actualPayloadLen);
+
+  // The `CRYPTO_ABYTES` of memory for the tag is not needed for plaintext.
+  aMessage.SetLength(aMessage.GetLength() - CRYPTO_ABYTES);
+
+  return OT_ERROR_NONE;
+}
+
+} // namespace Mle
+} // namespace ot
